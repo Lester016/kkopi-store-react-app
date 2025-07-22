1a05c1f8256bcb3d8f52299d38e38194ec4c93d9
Lester <lesterdeguzman1999@gmail.com>
2025-07-22 10:44:34 +0800

    feat: Update axios token referrence

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,16 +1,17 @@
-import axios from "axios";
+import axios from 'axios';
+import { useAuthStore } from '../store/authStore';
 
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
   timeout: 10000,
   headers: {
-    "Content-Type": "application/json",
+    'Content-Type': 'application/json',
   },
 });
 
 // Optionally handle token attach globally (interceptors)
 axiosInstance.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token"); // Or use context/store
+  const token = useAuthStore.getState().token;
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
