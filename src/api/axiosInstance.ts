e466b339f78bfdccbf0e0761e2c90da324eee605
Lester <lesterdeguzman1999@gmail.com>
2025-07-27 19:42:25 +0800

    feat: Add refresh token middleware for expired access token validation wiht axiosIntance

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { useAuthStore } from '../store/authStore';
+import { useAuthStore, type IToken } from '../store/authStore';
 
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
@@ -13,9 +13,51 @@ const axiosInstance = axios.create({
 axiosInstance.interceptors.request.use((config) => {
   const token = useAuthStore.getState().token;
   if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+    config.headers.Authorization = `Bearer ${token.access}`;
   }
   return config;
 });
+
+axiosInstance.interceptors.response.use(
+  (res) => res,
+  async (error) => {
+    const token = useAuthStore.getState().token;
+    const setAuth = useAuthStore.getState().setAuth;
+    const clearAuth = useAuthStore.getState().clearAuth;
+    const originalRequest = error.config;
+
+    // Prevent infinite loop
+    if (error.response?.status === 401 && !originalRequest._retry && token?.refresh) {
+      originalRequest._retry = true;
+
+      try {
+        const response = await axios.post(`${import.meta.env.VITE_API_BASE_URL}/api/refresh`, {
+          refresh: token?.refresh,
+        });
+        const { access, refresh } = (await response.data) as IToken;
+        setAuth({ access, refresh });
+        originalRequest.headers.Authorization = `Bearer ${access}`;
+        console.log('Token refreshed successfully:', access);
+        return axiosInstance(originalRequest);
+      } catch (error: unknown) {
+        clearAuth();
+        logError(error, 'Requesting refresh token error');
+        return Promise.reject(error);
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
+
+const logError = (error: unknown, context = '') => {
+  if (axios.isAxiosError(error)) {
+    console.log(`${context}:`, error.response?.data?.error || error.message);
+  } else if (error instanceof Error) {
+    console.log(`${context}:`, error.message);
+  } else {
+    console.log(`${context}:`, error);
+  }
+};
 
 export default axiosInstance;
