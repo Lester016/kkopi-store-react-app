0f7fd42304d044e7e8cbe29a9702ae8f14d659e4
Lester <lesterdeguzman1999@gmail.com>
2025-08-28 16:52:25 +0800

    feat: Use gated condition for showing NavBar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,15 @@
 import NavBar from '../shared/components/NavBar';
+import { useAuthStore } from '../store/authStore';
 import AppRouter from './router';
 
 const App = () => {
+  const { user } = useAuthStore();
+
   return (
     <div className="min-h-screen flex flex-col bg-gray-50">
-      <NavBar />
+      {user && <NavBar />}
 
       <main className="flex-1 overflow-auto p-6">
-        {/* <Outlet /> Renders child route here */}
         <AppRouter />
       </main>
     </div>
