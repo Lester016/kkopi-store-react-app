f057b64b79dde47b5ff58db26b009144c5f61a0c
Lester <lesterdeguzman1999@gmail.com>
2025-08-29 20:45:54 +0800

    feat: Create login service

---
+++ src/features/auth/services/login.ts
@@ -0,0 +1,7 @@
+import axiosInstance from '../../../api/axiosInstance';
+import type { LoginFormInputs } from '../types/login.types';
+
+export const login = async (payload: LoginFormInputs) => {
+  const { data } = await axiosInstance.post('/api/login', payload);
+  return data; // { access, refresh }
+};
