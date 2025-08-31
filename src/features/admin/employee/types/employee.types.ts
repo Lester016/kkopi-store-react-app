e817dc2be7acdd5090cb1c053eb014fc810b549e
Lester <lesterdeguzman1999@gmail.com>
2025-08-31 18:15:55 +0800

    feat: Create Update employee form input types

---
+++ src/features/admin/employee/types/employee.types.ts
@@ -0,0 +1,10 @@
+export interface EmployeeFormInputs {
+  email: string;
+  password: string;
+  firstName: string;
+  lastName: string;
+  position: string;
+  branch: string;
+  startDate: string; // YYYY-MM-DD
+  phone: string;
+}
